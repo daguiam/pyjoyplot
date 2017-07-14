f88cde09d08bc004c2b1bcd357b3de31e2499937
daguiam <diogoaguiam@gmail.com>
2017-07-14 15:21:58 +0100

    update

--- joyplot.py
+++ joyplot.py
@@ -4,11 +4,16 @@
 
 import matplotlib.pylab as plt
 import numpy as np
+import pandas as  pd
 
 def joyplot(data,
             labels=None,
             overlap=0.5,
             fill=False,
+            color=None,
+            facecolor=None,
+            edgecolor=None,
+            alpha=None,
             ax=None):
     """ joyplot
 
@@ -39,33 +44,96 @@ def joyplot(data,
     maxydata = np.max(ydata)
     data[:,1] = ydata/maxydata
     
-    
     totalplots = len(data)
-    ylabelpoints = list(reversed(range(totalplots)))
+    ylabelpoints = np.array(list(reversed(range(totalplots))))
+    handles = []
     for i in range(totalplots):
         
         x,y = data[i]
         label = labels[i]
-        print label#,,y
+        #print label#,,y
         ystart = ylabelpoints[i]
-        y = y*(overlap)+ystart
+        y = y*(overlap)+ystart+0.05
         if fill:
-            ax.fill_between(x, ystart, y)
+            hax = ax.fill_between(x, ystart, y, 
+                        interpolate=True)
+
         else:
-            ax.plot(x, y)
-    ax.set_yticks(ylabelpoints)
+            hax = ax.plot(x, y)
+        handles.append(hax)
+    for hax in handles:
+        if color is not None:
+                hax.set_color(color)
+        if fill:
+            if facecolor is not None:
+                    hax.set_facecolor(facecolor)
+            if edgecolor is not None:
+                    hax.set_edgecolor(edgecolor)
+        if alpha is not None:
+                hax.set_alpha(alpha)
+
+    ax.set_yticks(np.array(ylabelpoints)+0.5)
     ax.set_yticklabels(labels)
-
+    ax.tick_params(axis='both',left=False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['top'].set_visible(False)
 if __name__ == "__main__":
-    x1 = np.linspace(0,24,100)
-    y1 = np.abs(np.random.randn(len(x1)))
-    #x2 = np.linspace(0,24,100)
-    y2 = np.abs(np.random.randn(len(x1)))
-    
-    labels = ['Data 1','Data 2']
-    data = [[x1, y1], [x1, y2]]
-    joyplot(data, labels=labels, overlap=1, fill=True)
-    
 
+    data = []
+    labels = []
+    for i in range(10):
+        x1 = np.linspace(0,24,100)
+        y1 = np.abs(np.random.randn(len(x1)))
+        y1 = np.abs(np.random.randn(len(x1)))
+
+        y1 = y1/np.max(y1)
+        
+        data.append([x1,y1])
+        labels.append('Data %d'%(i))
+    
+    # ETL
+    if 1:
+        data = []
+        labels = []
+        df = pd.read_fwf('birthsRR.txt',widths=[4,5,8,12])
+        print df
+        #df = pd.read_fwf('birthsTR.txt')
+        year = '2013'
+        yd = df.loc[df['Year']==year]
+
+        
+        countries = sorted(list(set(yd['Code'])))
+        for code in countries:
+            cd = yd.loc[df['Code']==code]
+            x = np.array(cd['Age'])
+            #x[x=='12-'] = '12'
+            #x[x=='55+'] = '55'
+            x = x.astype(np.int)
+            
+            y = np.array(cd['Total']).astype(np.float)
+            if code == 'USA':
+                print cd
+                print 'USA', zip(x,y)
+            y = y/np.max(y)
+
+            labels.append(code)
+            data.append([x,y])
+
+
+    #labels = ['Data 1','Data 2']
+    #data = [[x1, y1], [x1, y2]]
+    joyplot(data, 
+            labels=labels, 
+            overlap=0.8, 
+            fill=True,
+            #color='C0',
+            facecolor='C0',
+            edgecolor='w',
+            alpha=1)
+
+    plt.xlabel('Age')
+    plt.title('Live births distribution by age in %s'%(year))
+    plt.xlim([12,55])
 
     plt.show()
--- readme.md
+++ readme.md
@@ -1,12 +1,22 @@
-joyplot using matplotlib
+pyjoyplot using matplotlib
 ---
 
-Joyplots are awesome! Joyplots 
+Joyplots are awesome! 
+Joyplot is the term recently coined for similar plots to the famous Joy Division album cover.
 
-![joyplot](joy-plot.jpg)
 
+Check the original post in [/r/dataisbeautiful/](https://www.reddit.com/r/dataisbeautiful/comments/6m0wo7/peak_time_for_sports_and_leisure_oc/)
 
+Joyplots have been done in [R](https://github.com/halhen/viz-pub/blob/master/sports-time-of-day/2_gen_chart.R)
+and [Excel](http://chandoo.org/wp/2017/07/12/joyplot-in-excel/)
+
+This is one implementation for joyplots in python using matplotlib.
+
+![joyplot](joyplot.png)
 
 https://eagereyes.org/blog/2017/joy-plots
+
+
+Birth rate data taken from [The Human Fertility Database](http://www.humanfertility.org/)
 
 
